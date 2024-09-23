a66290f70d0a6d69f35023045c301ae6745c38bb
kindabrazy <kindofbrazy@gmail.com>
2024-09-23 20:06:17 +0330

    chore(oneTrainer): update OneTrainer background image chore(oneTrainer): remove catchAddress from OneTrainer fix(modules): correct module version

--- src/Container/Nerogar/RendererMethods.ts
+++ src/Container/Nerogar/RendererMethods.ts
@@ -1,7 +1,6 @@
 import {CardRendererMethods} from '../../types';
-import {catchAddress} from '../../Utils/RendererUtils';
 import {startInstall} from './Install';
 
-const nerogarRendererMethods: CardRendererMethods = {catchAddress, installUI: {startInstall}};
+const nerogarRendererMethods: CardRendererMethods = {installUI: {startInstall}};
 
 export default nerogarRendererMethods;
--- src/renderer.ts
+++ src/renderer.ts
@@ -18,8 +18,8 @@ export function setCurrentBuild(build: number) {
           repoUrl: 'https://github.com/Nerogar/OneTrainer',
           type: 'image',
           bgUrl:
-            'https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/db3575cb-162a-4436-b5d0-f9fbf9ed1140' +
-            '/width=300/00002-4073703889.jpeg',
+            'https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/4f810fe1-775b-44c4-83f0-f1ad07c8fb09' +
+            '/width=300/00005-1318253062.jpeg',
           methods: nerogarRendererMethods,
         });
       }

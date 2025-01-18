39dcae85420bc1f57df5ed2d51b65ecf1113fab9
kindabrazy <kindofbrazy@gmail.com>
2025-01-18 15:27:18 +0330

    update index.js imports

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-import path from 'node:path';
+import {dirname, join} from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-import fs from 'graceful-fs';
+import {promises} from 'graceful-fs';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
-const compiledPath = path.join(__dirname, '..', 'Compiled');
+const compiledPath = join(__dirname, '..', 'Compiled');
 
 async function copyDirectory(src, dest) {
-  await fs.promises.mkdir(dest, {recursive: true});
-  const entries = await fs.promises.readdir(src, {withFileTypes: true});
+  await promises.mkdir(dest, {recursive: true});
+  const entries = await promises.readdir(src, {withFileTypes: true});
 
   for (let entry of entries) {
-    const srcPath = path.join(src, entry.name);
-    const destPath = path.join(dest, entry.name);
+    const srcPath = join(src, entry.name);
+    const destPath = join(dest, entry.name);
 
     if (entry.isDirectory()) {
       await copyDirectory(srcPath, destPath);
     } else {
-      await fs.promises.copyFile(srcPath, destPath);
+      await promises.copyFile(srcPath, destPath);
     }
   }
 }
 
 async function main() {
   try {
-    // Remove existing "scripts" folder and "lynxModule.json" file in compiledPath
-    await fs.promises.rm(path.join(compiledPath, 'scripts'), {recursive: true, force: true});
-    await fs.promises.unlink(path.join(compiledPath, 'lynxModule.json')).catch(() => {});
+    await promises.rm(join(compiledPath, 'scripts'), {recursive: true, force: true});
+    await promises.unlink(join(compiledPath, 'lynxModule.json')).catch(() => {});
 
-    // Copy "scripts" folder from current directory to compiledPath
-    await copyDirectory(path.join(__dirname, 'scripts'), path.join(compiledPath, 'scripts'));
+    await copyDirectory(join(__dirname, 'scripts'), join(compiledPath, 'scripts'));
 
-    // Copy "lynxModule.json" file from current directory to compiledPath
-    await fs.promises.copyFile(path.join(__dirname, 'lynxModule.json'), path.join(compiledPath, 'lynxModule.json'));
+    await promises.copyFile(join(__dirname, 'lynxModule.json'), join(compiledPath, 'lynxModule.json'));
 
     console.log('Files and folders copied successfully!');
   } catch (error) {

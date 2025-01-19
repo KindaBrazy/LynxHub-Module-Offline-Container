40315a9e0c22653ccade7a57009aa94d063fd878
kindabrazy <kindofbrazy@gmail.com>
2025-01-19 17:50:32 +0330

    fix(invokeai): resolve issue with reading and writing arguments

--- src/Container/Image/InvokeAI/MainMethods.ts
+++ src/Container/Image/InvokeAI/MainMethods.ts
@@ -15,12 +15,12 @@ async function getRunCommands(dir?: string): Promise<string | string[]> {
   return await utilRunCommands(BAT_FILE_NAME, dir, DEFAULT_BATCH_DATA);
 }
 
-export async function saveArgs(cardDir: string, args: ChosenArgument[]) {
-  return await utilSaveArgs(cardDir, args, BAT_FILE_NAME, parseArgsToString);
+export async function saveArgs(args: ChosenArgument[], cardDir?: string) {
+  return await utilSaveArgs(args, BAT_FILE_NAME, parseArgsToString, cardDir);
 }
 
-export async function readArgs(cardDir: string) {
-  return await utilReadArgs(cardDir, BAT_FILE_NAME, DEFAULT_BATCH_DATA, parseStringToArgs);
+export async function readArgs(cardDir?: string) {
+  return await utilReadArgs(BAT_FILE_NAME, DEFAULT_BATCH_DATA, parseStringToArgs, cardDir);
 }
 
 async function getLatest(): Promise<ReleaseInfo | null> {
@@ -43,6 +43,6 @@ function mainIpc(ipc: MainIpcTypes) {
   ipc.handle('get-latest', getLatest);
 }
 
-const invokeMainMethods: CardMainMethods = {getRunCommands, updateAvailable, mainIpc};
+const invokeMainMethods: CardMainMethods = {getRunCommands, updateAvailable, mainIpc, saveArgs, readArgs};
 
 export default invokeMainMethods;

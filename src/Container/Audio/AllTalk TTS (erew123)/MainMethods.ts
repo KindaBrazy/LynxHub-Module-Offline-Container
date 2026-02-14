ccbaae2c42d02013c746eaeb9eec569ff33aca78
KindaBrazy <kindofbrazy@gmail.com>
2026-02-14 10:26:29 +0330

    fix(Audio): Improve AllTalk TTS installation check

--- src/Container/Audio/AllTalk TTS (erew123)/MainMethods.ts
+++ src/Container/Audio/AllTalk TTS (erew123)/MainMethods.ts
@@ -1,7 +1,7 @@
 import {CardMainMethodsInitial} from '../../../../../src/common/types/plugins/modules';
 import {ALLTALK_ID} from '../../../Constants';
 import {isWin} from '../../../Utils/CrossUtils';
-import {isGitRoot, utilRunCommands} from '../../../Utils/MainUtils';
+import {checkFilesExist, isGitRoot, utilRunCommands} from '../../../Utils/MainUtils';
 
 const BAT_FILE_NAME = isWin ? 'start_alltalk.bat' : 'start_alltalk.sh';
 
@@ -12,7 +12,10 @@ export async function getRunCommands(): Promise<string | string[]> {
 async function isInstalled(dir: string | undefined) {
   if (!dir) return false;
 
-  return isGitRoot(dir, 'https://github.com/erew123/alltalk_tts');
+  const isRepo = await isGitRoot(dir, 'https://github.com/erew123/alltalk_tts');
+  if (isRepo) return true;
+
+  return checkFilesExist(dir, [BAT_FILE_NAME]);
 }
 
 const Rrew123_MM: CardMainMethodsInitial = utils => {
